99a41c325b4b305718a6d10e303d1b8c50d5625f
Sam Blackshear <samuel.blackshear@colorado.edu>
2013-03-03 23:59:10 -0700

    updating test build script for immutability tests

--- build/build_tests.sh
+++ build/build_tests.sh
@@ -13,3 +13,13 @@ do
     make
     cd $BASE/apps/tests/regression
 done
+
+cd $BASE/apps/tests/immutability
+for dir in $( ls -d */);
+do
+    echo 'Building' $dir
+    cd $dir 
+    mkdir bin
+    make
+    cd $BASE/apps/tests/immutability
+done
