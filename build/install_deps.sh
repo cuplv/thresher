bd6470698163af4249efbf9574e62cda2104b20b
Sam Blackshear <samuel.blackshear@colorado.edu>
2013-06-27 10:36:55 -0600

    simple find/replace fix to allow WALA to compile with older versions of Java

--- build/install_deps.sh
+++ build/install_deps.sh
@@ -3,6 +3,7 @@
 # should be run only from Makefile in Thresher root directory
 
 # get WALA
-git clone https://github.com/wala/WALA.git && mv WALA lib
+git clone https://github.com/wala/WALA.git && mv WALA lib && sed -i 's/private/protected/g' lib/WALA/com.ibm.wala.util/src/com/ibm/wala/fixpoint/IntSetVariable.java
+# last command is a bad hack to prevent compilation problems on older jvms
 
 
