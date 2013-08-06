c77e800d6b21910c621ff3860d1c2af6da35ded8
Sam Blackshear <samuel.blackshear@colorado.edu>
2013-08-06 14:17:02 -0600

    fixing button parsing to take advantage of public.xml file emitted by apktool

--- src/edu/colorado/thresher/core/AndroidUtils.java
+++ src/edu/colorado/thresher/core/AndroidUtils.java
@@ -50,71 +50,6 @@ public class AndroidUtils {
   
   private final static String DEFAULT_LISTENER = "onClick";
 
-	public static void main(String[] args) {
-		/*
-		Collection<AndroidButton> buttons = parseButtonInfo(args[0]);
-		for (AndroidButton button : buttons) {
-			System.out.println("Button " + button);
-		}*/
-		
-		buildSyntacticCG(args[0]);
-
-		// inputs: list of external code callers (reflection, serializiation, ...), entrypoints?
-		
-
-	}
-    /*	
-    static class Util {
-		public static Collection<File> listFilesRec(File startDir) {
-			Util.Pre(startDir.isDirectory());
-			Collection<File> files = new ArrayList<File>();
-			
-			final File[] genFiles = startDir.listFiles();
-			for (File f : genFiles) {
-				if (f.isDirectory()) {
-					files.addAll(listFilesRec(f));
-				} else { // is file
-					files.add(f); 
-				}
-			}
-			return files;
-		}
-		
-		public static void Assert(boolean b, String s) {
-			if (!b) {
-				System.out.println("Failed assertion: " + s);
-				Thread.dumpStack();
-			}
-		}
-		
-		public static void Assert(boolean b) {
-			Assert(b, "");
-		}
-		
-		public static void Unimp(String s) {
-			Assert(false, s);
-		}
-		
-		public static void Pre(boolean b) {
-			Assert(b, "Failed precondition: ");
-		}
-	}
-
-    // TMP!
-    static class CGNode {
-
-    }
-
-    static class HashMapFactory {
-	public static HashMap make() { return new LinkedHashMap(); }
-    }
-
-    static class HashSetFactory {
-	public static HashSet make() { return new LinkedHashSet(); }
-    }
-    // END TMP
-    */
-  
   static class AndroidButton {
     // unique identifier for the button
     final String id;
@@ -141,9 +76,8 @@ public class AndroidUtils {
     }
     
     public String toString() {
-	return "ID: \"" + id + " " + intId + "\" Handler: \"" + eventHandlerName + "\" Label: \"" + label + "\" stringName: \"" + buttonStringId + "\"" 
-	//+ " handler nodes " + Util.printCollection(eventHandlerNodes);
-		;// TMP!
+      return "ID: \"" + id + " " + intId + "\" Handler: \"" + eventHandlerName + "\" Label: \"" + label + "\" stringName: \"" + buttonStringId + "\"" 
+          + " handler nodes " + Util.printCollection(eventHandlerNodes);
     }
         
     public boolean hasDefaultListener() {
@@ -256,7 +190,51 @@ public class AndroidUtils {
       ioe.printStackTrace();
     } 
     
-    parseIntIds(buttonIdMap, appPath); 
+    // read button id's from res/values/public.xml, if it exists
+    File publicXML = new File(appPath + "res/values/public.xml");
+    if (publicXML.exists()) {
+      try {
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(publicXML);
+        Element root = doc.getDocumentElement();
+        // get all button id's
+        NodeList nl = root.getElementsByTagName("public");
+                    
+        if (nl != null) {
+          for (int i = 0; i < nl.getLength(); i++) { // for each string
+            Element el = (Element) nl.item(i);
+            String type = el.getAttribute("type");
+            if (type.equals("id")) {
+              String name = el.getAttribute("name");
+              AndroidButton button = buttonStringMap.get(name);   
+              if (button != null) {
+                // found the button; now get and parse its id
+                String id = el.getAttribute("id");
+                int radix = 10;
+                if (id.startsWith("0x")) {
+                  // Java doesn't like parsing hexes that start with 0x. strip it out
+                  id = id.substring(2, id.length());
+                  radix = 16; // indicate that this is a hex value
+                }
+                Util.Assert(!id.contains("0x"));
+                // get integer id assigned to button
+                int intValue = Integer.parseInt(id, radix);
+                button.intId = intValue;
+              }
+            }
+          }
+        }
+      } catch(ParserConfigurationException pce) {
+        pce.printStackTrace();
+      } catch(SAXException se) {
+        se.printStackTrace();
+      } catch(IOException ioe) {
+        ioe.printStackTrace();
+      } 
+    } else {
+      // otherwise, try to parse them from the .class files generated from R directly
+      parseIntIds(buttonIdMap, appPath);
+    }
     
     // make sure we've found the labels and int id's for all buttons
     for (AndroidButton button : buttonIdMap.values()) {
@@ -266,15 +244,9 @@ public class AndroidUtils {
     
     return buttonIdMap.values();
   }
-	
-	static void parseStrings(Map<String,AndroidButton> buttonIdMap, String appPath) {
-		final File valuesDir = new File(appPath + "res/values/strings.xml");
-		Util.Assert(valuesDir.exists());
-		
-	}
 
+  // copied from Shrike
 	public static final byte CONSTANT_Integer = 3;
-
 	
 	static void buildSyntacticCG(String appPath) {
 	  String path = appPath + "bin/classes";
@@ -314,7 +286,7 @@ public class AndroidUtils {
 	* find R$id.class, parse out button ID's from the constant pool using Shrike
 	*/
 	static void parseIntIds(Map<String,AndroidButton> buttonIdMap, String appPath) {
-		String path = appPath + "bin/classes";
+		String path = appPath + "bin";
 		final File binDir = new File(path);
 		Util.Assert(binDir.exists(), "can't find " + path);
 		final Collection<File> binFiles = Util.listFilesRec(binDir);
@@ -354,181 +326,5 @@ public class AndroidUtils {
 				}
 			}
 		}
-
 	}
-	
-  /**
-   * read in R.java and populate the intId field of each button with its
-   * identifier from R.java
-   */
-  // TODO: this is syntactic and quite hacky... but I don't think it's
-  // a good idea to try to have WALA do it because it's very dependent
-  // on string values
-	static void parseInntIds(Map<String,AndroidButton> buttonIdMap, String appPath) {
-    final String INT_DECL = "public static final int ";
-    final String ID_CLASS = "public static final class id";
-    final File genDir = new File(appPath + "gen/");
-    Util.Assert(genDir.exists());
-    final Collection<File> genFiles = Util.listFilesRec(genDir);
-    
-    for (File f : genFiles) {
-      if (f.getName().equals("R.java")) {
-        try {
-          FileInputStream stream = new FileInputStream(f);
-          BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-          String line;
-          boolean parsing = false;
-          while ((line = br.readLine()) != null) {
-            //Util.Debug("line " + line);
-            if (line.contains(ID_CLASS)) {
-              parsing = true;
-            } else if (parsing && line.contains("}")) parsing = false;
-            
-            if (parsing && line.contains(INT_DECL)) {
-              line = line.replace(INT_DECL, "");
-              // strip out spaces, tabs, and semicolons
-              line =  line.replaceAll("[ \t;]", "");
-              int eqIndex = line.indexOf("=");
-              // get var name; should be between beginning of str and = sign
-              String varName = line.substring(0, eqIndex);
-              AndroidButton button = buttonIdMap.get(varName);
-              if (button != null) {
-                // parse out RHS of expression (part after = sign)
-                line = line.substring(eqIndex + 1, line.length());
-                int radix = 10;
-                if (line.startsWith("0x")) {
-                  // Java doesn't like parsing hexes that start with 0x. strip it out
-                  line = line.substring(2, line.length());
-                  radix = 16; // indicate that this is a hex value
-                }
-                Util.Assert(!line.contains("0x"));
-                // get integer id assigned to button
-                int intValue = Integer.parseInt(line, radix);
-                button.intId = intValue;
-              }
-            }
-          }
-        } catch (FileNotFoundException e) {
-          e.printStackTrace();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-  }
-    
-  // TODO: should incorporate Activity name into button name...otherwise we might get
-  // some extra matches in the case of unfortunate method names
-  /*
-  public static Map<String,String> parseButtonInfo(String appPath) {
-    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    String[] handlerStrs = new String[] { "android:onClick" };
-    Set<String> handlerNames = new LinkedHashSet<String>();
-    for (int i = 0; i < handlerStrs.length; i++) handlerNames.add(handlerStrs[i]);
-    // map from event handler name -> application methods that overrides event handler
-    Map<String,String> applicationOverridesMap = new LinkedHashMap<String,String>();
-    // map from button event handler -> string containing name for button
-    Map<String,String> buttonNamesMap = new LinkedHashMap<String, String>();
-    
-    final String BUTTON_ID = "android:id";
-    final String HANDLER_INDICATOR = "android:on";  
-    final String BUTTON_NAME = "android:text";
-    
-    // for each file in res/layout
-    final File layoutFolder = new File(appPath + "layout");
-    final File[] layoutFiles = layoutFolder.listFiles();
-    for (int f = 0; f < layoutFiles.length; f++) {
-      if (!layoutFiles[f].getName().endsWith(".xml")) continue;
-      try {
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document doc = db.parse(layoutFiles[f]);
-        Element root = doc.getDocumentElement();
-        // get all declared buttons
-        NodeList nl = root.getElementsByTagName("Button");
-          
-        if (nl != null) {
-          for (int i = 0; i < nl.getLength(); i++) { // for each button
-            Element el = (Element) nl.item(i);
-            NamedNodeMap map = el.getAttributes();
-            if (map == null) continue;
-            String handlerName = null;
-            for (int j = 0; j < map.getLength(); j++) {
-              Node node = map.item(j);
-              String label = node.getNodeName();
-
-              if (label.equals(BUTTON_ID)) {
-                final String buttonId = node.getNodeValue();
-                
-              }
-              
-              if (handlerNames.contains(label)) {
-                handlerName = node.getNodeValue();
-                applicationOverridesMap.put(label, handlerName);
-              } else if (label.startsWith(HANDLER_INDICATOR)) {
-                Util.Unimp("possibly unknown handler " + label);
-              }
-  
-              // TODO: need to associate node name with onClick; otherwise keys can stomp on each other
-              
-              if (label.equals(BUTTON_NAME)) {
-                Util.Assert(handlerName != null, "handler should be defined here!");
-                // add name of string containing button text to the map
-                String link = node.getNodeValue().replace("@string/", "");
-                buttonNamesMap.put(handlerName, link);
-              }
-            }
-          }
-        }
-      } catch(ParserConfigurationException pce) {
-        pce.printStackTrace();
-      } catch(SAXException se) {
-        se.printStackTrace();
-      } catch(IOException ioe) {
-        ioe.printStackTrace();
-      }
-    }
-    
-    // read button strings from res/values/strings.xml
-    try {
-      DocumentBuilder db = dbf.newDocumentBuilder();
-      Document doc = db.parse(appPath + "values/strings.xml");
-      Element root = doc.getDocumentElement();
-      // get all declared buttons
-      NodeList nl = root.getElementsByTagName("string");
-                  
-      if (nl != null) {
-        Set<String> assigned = HashSetFactory.make();
-        for (int i = 0; i < nl.getLength(); i++) { // for each string
-          Element el = (Element) nl.item(i);
-          String name = el.getAttribute("name");
-          String lab = null, newVal = null;
-          for (String label : buttonNamesMap.keySet()) {
-            if (name.equals(buttonNamesMap.get(label)) && assigned.add(label)) {
-              lab = label;
-              newVal = el.getTextContent();
-            }
-          }
-          
-          if (newVal != null) {
-            buttonNamesMap.put(lab, newVal);
-          }
-        }
-        // make sure we've found the names for all buttons
-        for (String label : buttonNamesMap.keySet()) {
-          if (!assigned.contains(label)) {
-            Util.Assert(false, "Missing button label for handler " + label);
-          }
-        }
-      }
-    } catch(ParserConfigurationException pce) {
-      pce.printStackTrace();
-    } catch(SAXException se) {
-      se.printStackTrace();
-    } catch(IOException ioe) {
-      ioe.printStackTrace();
-    } 
-
-    return buttonNamesMap;
-  }
-  */
 }
