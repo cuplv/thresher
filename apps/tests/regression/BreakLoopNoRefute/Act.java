d7abb74e25f3d95f6dd4ad436f8d003ca96715a8
Sam Blackshear <samuel.blackshear@colorado.edu>
2013-08-06 13:36:49 -0600

    new tests for loops w/ breaks

---
+++ apps/tests/regression/BreakLoopNoRefute/Act.java
@@ -0,0 +1,16 @@
+import edu.colorado.thresher.external.Annotations.*;
+
+@noStaticRef public class Act {
+
+    public Act() {}
+    
+    public static FakeMap storyCache = new FakeMap();
+
+    public static void main(String[] args) {
+	FakeMap localMap = new FakeMap();
+	//localMap.put("a", new Act(), 2, 3);
+	localMap.put("a", new Object());
+	storyCache.put("b", new Object());
+	//storyCache.put(new Object());
+    }
+}
