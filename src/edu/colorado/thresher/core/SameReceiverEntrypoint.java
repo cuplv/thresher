ef2e08b801c196af0cd485474bca9052c7e6132d
Sam Blackshear <sblackshear@github.com>
2014-02-24 09:57:55 -0700

    bug fixes in SameReceiverEntrypoint

--- src/edu/colorado/thresher/core/SameReceiverEntrypoint.java
+++ src/edu/colorado/thresher/core/SameReceiverEntrypoint.java
@@ -2,10 +2,6 @@ package edu.colorado.thresher.core;
 
 import java.util.Map;
 
-import com.ibm.wala.analysis.typeInference.ConeType;
-import com.ibm.wala.analysis.typeInference.PrimitiveType;
-import com.ibm.wala.analysis.typeInference.TypeAbstraction;
-import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.ipa.callgraph.impl.AbstractRootMethod;
 import com.ibm.wala.ipa.callgraph.impl.DefaultEntrypoint;
@@ -35,58 +31,40 @@ import com.ibm.wala.util.collections.HashMapFactory;
  */
 public class SameReceiverEntrypoint extends DefaultEntrypoint {
   
-  private static final Map<TypeReference,Integer> cachedReceivers = HashMapFactory.make();
-    
-  private final int RECEIVER = 0;
-  
+  private static final Map<TypeReference,Integer> cachedArgs = HashMapFactory.make();
+  public static void clearCachedArgs() { cachedArgs.clear(); }
+      
   public SameReceiverEntrypoint(IMethod method, IClassHierarchy cha) {
     super(method, cha);
   }
 
-  private int getReceiver(TypeReference ref, AbstractRootMethod m) {
-    Integer result = cachedReceivers.get(ref);
-    int receiver;
-    if (result == null) {
-      // hack! needed for when WALA doesn't know how to create an instance of something
-      try {
-        // haven't seen a receiver of type ref yet; allocate a new one
+  protected int getOrCreateCachedArg(TypeReference ref, AbstractRootMethod m) {
+    if (ref.isPrimitiveType()) return m.addLocal(); // create fresh locals for primitive types
+    else {
+      Integer result = cachedArgs.get(ref);
+      int arg;
+      if (result == null) {
+        // haven't seen an arg of type ref yet; allocate a new one
+        // TODO: calling addAllocation invokes the default constructor for ref, but we may want to try a bit harder and invoke a more interesting one
         SSANewInstruction n = m.addAllocation(ref);
-      //receiver = (n == null) ? -1 : n.getDef();
-        receiver = n.getDef();
-        cachedReceivers.put(ref, receiver);
-      } catch (NullPointerException e) {
-        return -1;
-      }
-    } else receiver = result.intValue();
-    Util.Post(receiver != -1, "bad receiver");
-    return receiver;
+        arg = n.getDef();
+        cachedArgs.put(ref, arg);
+      } else arg = result.intValue();
+      Util.Post(arg != -1, "bad arg");
+      return arg;
+    }
   }
  
   @Override
   protected int makeArgument(AbstractRootMethod m, int i) {
     TypeReference[] p = getParameterTypes(i);
-    if (p.length == 0) {
-      return -1;
-    } else if (p.length == 1) {
-      if (p[0].isPrimitiveType()) {
-        return m.addLocal();
-      } else {
-        //SSANewInstruction n = m.addAllocation(p[0]);
-        //return (n == null) ? -1 : n.getDef();
-        return getReceiver(p[0], m);
-      }
-    } else {
+    if (p.length == 0) return -1;
+    else if (p.length == 1) return getOrCreateCachedArg(p[0], m);
+    else {
       int[] values = new int[p.length];
       int countErrors = 0;
       for (int j = 0; j < p.length; j++) {
-        int value;
-        if (j == RECEIVER) {
-          value = getReceiver(p[j], m);
-        } else {
-          // TODO: cache p[j] and don't add new allocation if we already have a cached version
-          SSANewInstruction n = m.addAllocation(p[j]);
-          value = (n == null) ? -1 : n.getDef();
-        }
+        int value = getOrCreateCachedArg(p[j], m);
         if (value == -1) {
           countErrors++;
         } else {
@@ -97,24 +75,7 @@ public class SameReceiverEntrypoint extends DefaultEntrypoint {
         int[] oldValues = values;
         values = new int[oldValues.length - countErrors];
         System.arraycopy(oldValues, 0, values, 0, values.length);
-      }
-
-      TypeAbstraction a;
-      if (p[0].isPrimitiveType()) {
-        a = PrimitiveType.getPrimitive(p[0]);
-        for (i = 1; i < p.length; i++) {
-          a = a.meet(PrimitiveType.getPrimitive(p[i]));
-        }
-      } else {
-        IClassHierarchy cha = m.getClassHierarchy();
-        IClass p0 = cha.lookupClass(p[0]);
-        a = new ConeType(p0);
-        for (i = 1; i < p.length; i++) {
-          IClass pi = cha.lookupClass(p[i]);
-          a = a.meet(new ConeType(pi));
-        }
-      }
-
+      }   
       return m.addPhi(values);
     }
   }
