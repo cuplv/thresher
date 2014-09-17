18b49d488fd32b8c8a9badb2c4394b929e6804d9
Sam Blackshear <sam.blackshear@gmail.com>
2014-09-17 09:32:01 -0600

    New tests for exceptional control flow

---
+++ apps/tests/casts/CatchRefute/Main.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+class Main {
+
+    public static void main(String[] args) {
+	Object x = new Object();
+	int i = 0;
+	try {
+	    String y = (String) x;
+	} catch (ClassCastException c) {}
+    }
+
+}
