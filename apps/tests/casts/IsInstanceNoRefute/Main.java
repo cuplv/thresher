5a65989275c10ae9741a10fd1e99c6293908b974
Sam Blackshear <sam.blackshear@gmail.com>
2014-09-24 21:10:00 -0600

    New tests for magic Class.isInstance method

---
+++ apps/tests/casts/IsInstanceNoRefute/Main.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+class Main {
+
+    public static void main(String[] args) {
+	SimpleInterface i;
+	
+	if (args[0] == "a") {
+	    i = new Foo();
+	} else {
+	    i = new Bar();
+	}
+
+	Class c = Bar.class;
+	if (c.isInstance(i)) {
+	    Foo foo = (Foo) i;
+	}
+    }
+
+}
